400d35298f93eff527e7d17dab4ead516ab834c9
juancarlosasensio <juancarlos@page-bird.com>
2021-06-07 12:19:47 -0400

    Add btn to clear current text in <textarea>s

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let form = document.querySelector('form');
 let csvField = document.getElementById('csv-text')
 let jsonField = document.getElementById('json-text')
+let clearCSVButton = document.querySelector('button[data-behavior^=clear-input]')
 const TEST_STR = `"Id","UserName"
 "1","Sam Smith"
 "2","Fred Frankly"
@@ -13,21 +14,25 @@ const TEST_ROW_DATA = [...TEST_ARR].slice(1)
 // console.log(TEST_ARR)
 // console.log(TEST_ROW_DATA)
 // console.log(TEST_HDRS)
+
+clearCSVButton.addEventListener('click', clearTextInput)
 csvField.addEventListener('input', getCSVString)
-form.addEventListener('submit', logCurrentCSVString)
+form.addEventListener('submit', displayCSVStringAsJSON)
     // What is the current value of the CSV string we will convert to JSON?
 
     //What do we need to do with this currentValue to be able to convert it to JSON?
     // -> Get column headers. For each record, get the value for each column
 
     //How do we do this?    
-function logCurrentCSVString(e) {
+function displayCSVStringAsJSON(e) {
   let currentCSVString = getCSVString(csvField);
   if (currentCSVString) { 
     let arr = csvStringToArray(currentCSVString);
     let headers = getCSVHeaders(arr);
     jsonField.value = buildJSON(headers, arr)
-  } else { console.log('Nothing to print') }
+  } else { 
+    jsonField.value = "";
+  }
   e.preventDefault();
 }
 
@@ -58,4 +63,19 @@ function getCSVHeaders(arr) {
 
 function getCSVString(input) {
   return String(input.value).trim();
+}
+
+//Clear input text
+function clearTextInput(e) {
+  e.preventDefault()
+  if (!e.target.dataset.behavior || e.target.dataset.behavior != 'clear-input-text' ) {
+    console.warn("Button element doesn't have data-attr.")
+    return;
+  } else {
+    let form = e.target.closest('form');
+    let inputName = e.target.dataset.clearInput;
+    let inputToClear = form.querySelector(`textarea[data-input-name~=${inputName}]`)
+    inputToClear.value = "";
+    jsonField.value = "";
+  }
 }
